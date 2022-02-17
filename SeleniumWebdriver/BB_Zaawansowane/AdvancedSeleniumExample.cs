d2d65031f6a7942d4398dd0c0693428166feed5a
Michal Dobrzycki <dobrzycki@gmail.com>
2022-02-17 17:17:00 +0100

    Advanced waits

--- SeleniumWebdriver/BB_Zaawansowane/AdvancedSeleniumExample.cs
+++ SeleniumWebdriver/BB_Zaawansowane/AdvancedSeleniumExample.cs
@@ -1,8 +1,10 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SeleniumWebdriver.BB_Zaawansowane
@@ -18,15 +20,37 @@ namespace SeleniumWebdriver.BB_Zaawansowane
         }
 
 
-        [Test]
-        public void TestCodeInIt()
+        [TestCase("Desktop", "C", "2", "12")]
+        [TestCase("Server", "Java", "3", "23")]
+        [TestCase("Server", "Java", "3", "23")]
+        public void TestCodeInIt(string category, string language, string valueId, string languageId)
         {
             driver.Url = "https://testpages.herokuapp.com/styled/basic-ajax-test.html";
+            IWebElement categoryDropdown = driver.FindElement(By.Id("combo1"));
+            SelectElement categorySelect = new SelectElement(categoryDropdown);
+
+            categorySelect.SelectByText(category);
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+            {
+                PollingInterval = TimeSpan.FromSeconds(1),
+            };
+
+            wait.Until(driver =>
+                !driver.FindElement(By.Id("ajaxBusy")).Displayed);
+
+            IWebElement languageDropDown = driver.FindElement(By.Id("combo2"));
+            SelectElement languageSelect = new SelectElement(languageDropDown);
+            languageSelect.SelectByText(language);
+
             IWebElement codeInItButton = driver.FindElement(By.Name("submitbutton"));
             codeInItButton.Click();
 
-            IWebElement valueId = driver.FindElement(By.Id("_valueid"));
-            StringAssert.AreEqualIgnoringCase(valueId.Text, "1");
+            IWebElement valueIdElement = driver.FindElement(By.Id("_valueid"));
+            StringAssert.AreEqualIgnoringCase(valueIdElement.Text, valueId);
+
+            IWebElement valueLanguageIdElement = driver.FindElement(By.Id("_valuelanguage_id"));
+            StringAssert.AreEqualIgnoringCase(valueLanguageIdElement.Text, languageId);
         }
 
         [Test]
@@ -51,6 +75,8 @@ namespace SeleniumWebdriver.BB_Zaawansowane
             IWebElement tableElement = driver.FindElement(By.Id("dynamictable"));
             IList<IWebElement> tableRows = tableElement.FindElements(By.XPath("//tr"));
             Console.Write(tableRows.Count);
+
+          
         }
     }
 }
