b8c42670997bc857a2c0d2aed7f918810c97b8d2
Michal Dobrzycki <dobrzycki@gmail.com>
2022-02-18 10:09:57 +0100

    Page Object Example

---
+++ SeleniumWebdriver/PageObjectPattern/pages/RegisterPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumWebdriver.PageObjectPattern.pages
+{
+    public class RegisterPage
+    {
+        private IWebDriver driver;
+
+        public RegisterPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private By userNameLocator => By.XPath("//form//input[@type='text']");
+        private By emailLocator => By.XPath("//form//input[@type='email']");
+        private By passwordLocator => By.XPath("//form//input[@type='password']");
+
+        public void CreateUser(string username, string email, string password)
+        {
+            driver.FindElement(userNameLocator).SendKeys(username);
+            driver.FindElement(emailLocator).SendKeys(email);
+            driver.FindElement(passwordLocator).SendKeys(password);
+        }
+    }
+}
---
+++ SeleniumWebdriver/PageObjectPattern/tests/RegisterUserTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumWebdriver.PageObjectPattern.pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumWebdriver.PageObjectPattern.tests
+{
+    public class RegisterUserTest
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestUserRegistration()
+        {
+            var timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+            string email = $"dobrzycki+{timestamp}@gmail.com";
+            string username = "John Wick";
+            string password = "Daisy";
+
+            RegisterPage registerPage = new RegisterPage(driver);
+            registerPage.CreateUser(username, email, password);
+            HomePage homePage = new HomePage(driver);
+            StringAssert.AreEqualIgnoringCase(username, homePage.getUserName());
+        }
+    }
+}
